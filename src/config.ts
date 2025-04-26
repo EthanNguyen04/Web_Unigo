65243e4e6881b4ab2c56fd8c6d3d227bcfceabb1
quannhph22518 <tuan2922020@gmail.com>
2025-04-26 17:24:28 +0700

    update order

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,26 @@
 // config.ts
 
 // Địa chỉ base của API
-export const BASE_URL = "http://192.168.1.7:3001";
+export const BASE_URL = "http://192.168.1.9:3001";
 
 // Các endpoint liên quan
 export const API_ADD_PRODUCT = `${BASE_URL}/manager/add_product`;
-export const API_ALL_CATEGORY = `${BASE_URL}/api/admin/all_category`;
+export const API_ALL_CATEGORY = `${BASE_URL}/api/category/all_category`;
+
+export const API_Get_CATEGORY = `${BASE_URL}/manager/categories`;
+
 export const API_dashboard = `${BASE_URL}/manager/dashboard`;
 export const API_LOGIN = `${BASE_URL}/api/user/login_admin`;
 export const API_LOGOUT = `${BASE_URL}/api/user/logout_admin`;
 export const API_PRODUCT_MANAGER = `${BASE_URL}/manager/products_manager`;
 
+export const API_GET_PRODUCT = `${BASE_URL}/api/product`;
+export const PUT_EDIT_PRODUCT = `${BASE_URL}/manager/edit_product`;
+
+export const API_GET_Edit_PRODUCT = `${BASE_URL}/manager/get_product`;
+
+export const Post_ADD_category = `${BASE_URL}/manager/add_category`;
+export const Edit_category = `${BASE_URL}/manager/edit_category`;
 
 
 // Các biến/config khác có thể thêm vào đây
